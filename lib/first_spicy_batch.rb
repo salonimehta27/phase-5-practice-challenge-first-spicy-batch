e4138eb3c65d917c1b75ba9d99061aaabdfc464a
Saloni <salonimehta2127@gmail.com>
2022-02-17 16:16:45 -0500

    commit tree d0775bb40b255f8385f718648a14b9c744558054

--- lib/first_spicy_batch.rb
+++ lib/first_spicy_batch.rb
@@ -4,5 +4,16 @@
 # def is_spicy_batch(batch):
 
 def first_spicy_batch(n)
-  # your code here
+  left = 0
+  right = n
+
+  while left < right
+    mid = left + (right - left) / 2
+    if is_spicy_batch(mid)
+      right = mid
+    else
+      left = mid + 1
+    end
+  end
+  left
 end
